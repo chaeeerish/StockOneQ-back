3c22852f72a7b5b591fc70db63654db446e63f08
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-04-17 03:08:08 +0900

    refactor: 불필요한 RefreshToken 도메인 삭제 -> Token으로 이동

--- src/main/java/umc/stockoneqback/auth/domain/repository/RefreshTokenRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.stockoneqback.auth.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import umc.stockoneqback.auth.domain.model.RefreshToken;
-
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Long> {
-}
