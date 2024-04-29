e6f8bab41c79323f23371fbd828da95a0473cc8e
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-04-29 14:50:36 +0900

    test: SecurityContext에 MockUser 집어넣기

---
+++ src/test/java/umc/stockoneqback/common/security/mock/WithCustomMockUserSecurityContextFactory.java
@@ -0,0 +1,25 @@
+package umc.stockoneqback.common.security.mock;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import umc.stockoneqback.global.security.principle.UserPrincipal;
+
+import java.util.List;
+
+public class WithCustomMockUserSecurityContextFactory implements WithSecurityContextFactory<WithCustomMockUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithCustomMockUser annotation) {
+        final UserPrincipal principal = new UserPrincipal(1L, "user_name", "user_login", "user_password", List.of(annotation.roleType().getValue()));
+        final UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(principal, "", principal.getAuthorities());
+
+//        String email = annotation.email();
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(authenticationToken);
+
+        return context;
+    }
+}
